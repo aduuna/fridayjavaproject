a5259ea4ec0453054081fe9df2bb2e64413b0736
Yaa-Providence-Design <providence.annor@turntabl.io>
2020-10-09 12:24:41 +0000

    test for getAverageGrade passed

--- src/friday/javaproject/Student.java
+++ src/friday/javaproject/Student.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 public class Student {
     ArrayList<Double> grades;
 
+    public ArrayList<Double> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(ArrayList<Double> grades) {
+        this.grades = grades;
+    }
+
     public double getAverageGrade(){
         double sum = 0.0;
         for(double num: this.grades){
