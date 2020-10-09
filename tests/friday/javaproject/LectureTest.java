e4239aaf0a00be07d961c1ce2c380ef7292ebdb6
Vincent Frimpong <vincent.frimpong@turntabl.io>
2020-10-09 15:00:28 +0000

    added tests for getAverageGrade

--- tests/friday/javaproject/LectureTest.java
+++ tests/friday/javaproject/LectureTest.java
@@ -1,12 +1,39 @@
 package friday.javaproject;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LectureTest {
 
     @org.junit.jupiter.api.Test
-    void getHighestAverageGrade() {
+    void testGetHighestAverageGrade() {
         Lecture science = new Lecture();
+
+        double[] averages = {0,0,0};
+
+
+        for(int i=0; i<3; i++){
+            Student student = new Student();
+            ArrayList<Double> grades = new ArrayList<>();
+            double sum = 0.0;
+            for(int j=0; j<5; j++){
+                double grade = Math.random()*100;
+                grades.add(grade);
+
+                sum += grade;
+
+            }
+            averages[i] = sum/5;
+            student.setGrades(grades);
+            science.enter(student);
+        }
+        
+        double highestAverage = science.getHighestAverageGrade();
+
+        double actualHighestAverage = Math.max( Math.max(averages[0], averages[1]), averages[2] );
+
+        assertEquals(highestAverage, actualHighestAverage);
 
     }
 }
